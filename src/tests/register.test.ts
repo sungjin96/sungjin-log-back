3e3be9931e8f162eb6f94e0c637ca1a61a737393
JJangseong <sungjin5891@gmail.com>
2020-05-17 20:06:02 +0900

    서버 테스트 코드 추가 및 서버 코드 분리

--- src/tests/register.test.ts
+++ src/tests/register.test.ts
@@ -1,30 +1,32 @@
 import { request } from "graphql-request";
 
-import { host } from "./constants";
-import { createConnection } from "typeorm";
 import { User } from "../entity/User";
+import { startServer } from "../startServer";
+import { AddressInfo } from "net";
 
-const email = "tob@bob.com";
-const password = "1234";
+let getHost = () => "";
+
+beforeAll(async () => {
+  const app = await startServer();
+  const { port } = app.address() as AddressInfo;
+  getHost = () => `http://127.0.0.1:${port}`;
+});
+
+const email = "tom@bob.com";
+const password = "jalksdf";
 
 const mutation = `
-  mutation {
-    register(email: "${email}", password: "${password}")
-  }
+mutation {
+  register(email: "${email}", password: "${password}")
+}
 `;
 
 test("Register user", async () => {
-  const response = await request(host, mutation);
+  const response = await request(getHost(), mutation);
   expect(response).toEqual({ register: true });
-  await createConnection();
   const users = await User.find({ where: { email } });
   expect(users).toHaveLength(1);
   const user = users[0];
   expect(user.email).toEqual(email);
   expect(user.password).not.toEqual(password);
 });
-
-
-// use a test database
-// drop all data once the test is over
-// when I run yarn test it also starts the server
