86fc68895f55ddca15f4fcd9b6a0892283ab27ef
JJangseong <sungjin5891@gmail.com>
2020-05-27 20:13:06 +0900

    cors 설정

--- src/startServer.ts
+++ src/startServer.ts
@@ -6,6 +6,14 @@ import { genSchema } from "./utils/genSchema";
 export const startServer = async () => {
   const server = new GraphQLServer({ schema: genSchema() as any });
   await createTypeormConn();
+  server.express.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+  });
   const app = await server.start({
     port: process.env.NODE_ENV === "test" ? 0 : 4000,
   });
